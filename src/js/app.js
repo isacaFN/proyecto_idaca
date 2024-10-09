d919b7b6cd891ff6d6fe4f8ac7fdf9ee6ed1547d
isaac caceres <isaac.caceres.169@gmail.com>
2024-10-09 18:59:59 -0300

    cambios al js

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,14 @@ async function consultarAPI(){
     try {
         const url = 'http://localhost/proyecto_idaca/public/api/usuarios';
         const respuesta = await fetch(url);
-        const usuarios = respuesta.json();
-        console.log(usuarios);
+        const usuarios = await respuesta.json();
+        mostrarUsuarios(usuarios);
     } catch (error) {
         console.log(error);
         
     }
 }
 
+function mostrarUsuarios(usuarios){
+    console.log(usuarios);
+}
